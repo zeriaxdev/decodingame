c35fc254d717a4e2f295fe6097e56a3fd85c078b
zeriaxdev <hedgegorg@gmail.com>
2022-08-06 23:14:53 +0300

    decodingame

---
+++ README.md
@@ -0,0 +1,5 @@
+# decodingame
+
+Once I was really bored and decided to make a thing. I encoded a string. 
+
+Your job is to decode it and PR the result. Good luck.
---
+++ play.sh
@@ -0,0 +1,11 @@
+GREEN="\033[32;1m"
+RESET="\033[0m"
+
+echo -e "$GREEN"
+echo "> Ready for an adventure?"
+sleep 1
+echo "> Let's go!"
+sleep 1
+echo "> Visit this website to start :)"
+echo -e "$RESET"
+echo ":: https://pastebin.fi/p/rNrDAFfdyX4k ::"
